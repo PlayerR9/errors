624fe5e3572bdd0ee7ac5f334eec73f463f0b93d
PlayerR9 <riateapad@gmail.com>
2024-09-27 14:51:44 +0200

    update

--- common.go
+++ common.go
@@ -3,9 +3,23 @@ package errors
 import (
 	"errors"
 	"fmt"
+	"io"
 
 	gcers "github.com/PlayerR9/go-errors/error"
+	gerr "github.com/PlayerR9/go-errors/error"
 )
+
+// DisplayError displays the complete error to the writer.
+//
+// Parameters:
+//   - w: The writer to write to.
+//   - err: The error to display.
+//
+// Returns:
+//   - error: The error that occurred while displaying the error.
+func DisplayError(w io.Writer, err error) error {
+	return gerr.DisplayError(w, err)
+}
 
 // Is is function that checks if an error is of type T.
 //
